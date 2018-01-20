2de19f3c2209d788342942d9b227826001751a68
xXLXx <leo.lavarias@gmail.com>
2018-01-20 09:00:58 +0800

    Add socket server

---
+++ socket-server/index.js
@@ -0,0 +1,16 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.send('<h1>Oops! Lost? This is a socket server</h1>');
+});
+
+var port = process.env('PORT');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
+io.on('connection', function(socket){
+  
+});
