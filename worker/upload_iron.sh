0ab81d59602cc0a1de7c8ce70d36c21a40540670
xXLXx <leo.lavarias@gmail.com>
2018-02-09 22:33:18 +0800

    Update socket server URL or worker

--- worker/upload_iron.sh
+++ worker/upload_iron.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Automatically replace all localhost in .env, assuming you have same configuration as server's
-sed -i '' 's/http:\/\/localhost/http:\/\/109.237.25.248/g' .env 
+sed -i '' 's/http:\/\/localhost/http:\/\/page-amp.com/g' .env 
 
 zip -r pageamp.zip .
 
-sed -i '' 's/http:\/\/109.237.25.248/http:\/\/localhost/g' .env 
+sed -i '' 's/http:\/\/page-amp.com/http:\/\/localhost/g' .env 
 
 iron worker upload --zip pageamp.zip --name pageamp_status_worker iron/php php Worker.php
